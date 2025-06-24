e05242c367f6290b35b0e458f05e13bc20eb65ef
iari4as <iariass@usm.cl>
2025-06-23 21:45:24 -0400

    error en core_tema y error en core_favorito

--- IA_Estudiantes/core/admin.py
+++ IA_Estudiantes/core/admin.py
@@ -1,2 +1,15 @@
 from django.contrib import admin
 
+from django.contrib import admin
+from .models import tema, favorito
+
+@admin.register(tema)
+class TemaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "nivel")
+    search_fields = ("nombre",)
+
+@admin.register(favorito)
+class FavoritoAdmin(admin.ModelAdmin):
+    list_display = ("usuario", "tema", "fecha_agregado")
+    list_filter = ("fecha_agregado",)
+    search_fields = ("usuario__username", "tema__nombre")
